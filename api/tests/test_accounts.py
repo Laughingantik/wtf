caae7ca227d8d5e6597b4a95bc1e56d3160e85d6
mrpudn <zach.mcauliffe@gmail.com>
2017-11-01 00:20:30 -0400

    Added account authentication

--- api/tests/test_accounts.py
+++ api/tests/test_accounts.py
@@ -1,12 +1,12 @@
-# pylint: disable=missing-docstring
-from mock import patch
-from api.accounts import Account
+# pylint: disable=missing-docstring,invalid-name
+from mock import patch, Mock
+from api.accounts import Account, AccountRepository
 
 
 @patch('api.accounts.uuid4')
-def test_account_defaults(uuid4):
+def test_account_defaults(mock_uuid4):
     # stub out uuid4 to return the same uuid for testing purposes
-    uuid4.return_value = '048e8cf5-bf6f-4b39-ac97-6f9851f61b16'
+    mock_uuid4.return_value = '048e8cf5-bf6f-4b39-ac97-6f9851f61b16'
     account = Account()
     assert account.uuid == '048e8cf5-bf6f-4b39-ac97-6f9851f61b16'
     assert account.email is None
@@ -28,12 +28,21 @@ def test_account_construction():
 
 
 @patch('api.accounts.sha256')
-def test_account_setters(sha256):
+def test_account_setters(mock_sha256):
     # stub out sha256 to return the same hashes for testing purposes
-    sha256.side_effect = [
-        '67d765888ea8f71875dfe27334786bffdca070705ee97bd17bec85f8580f7f01',
-        '012eab80b72cbfe663429219e920aee8cd17ba8893e302844682104ee88d3145'
-    ]
+    hash1 = Mock()
+    hash1.hexdigest = Mock(
+        return_value=(
+            '67d765888ea8f71875dfe27334786bffdca070705ee97bd17bec85f8580f7f01'
+        )
+    )
+    hash2 = Mock()
+    hash2.hexdigest = Mock(
+        return_value=(
+            '012eab80b72cbfe663429219e920aee8cd17ba8893e302844682104ee88d3145'
+        )
+    )
+    mock_sha256.side_effect = [hash1, hash2]
     account = Account()
     account.uuid = '60d0d4a4-3159-467d-a972-cd8a386931c4'
     assert account.uuid == '60d0d4a4-3159-467d-a972-cd8a386931c4'
@@ -46,3 +55,53 @@ def test_account_setters(sha256):
     )
     account.username = 'foobar'
     assert account.username == 'foobar'
+
+
+def test_find_account_by_uuid():
+    account = Account()
+    account.username = 'foobar'
+    repo = AccountRepository()
+    repo.save(account)
+    assert repo.find_by_uuid(account.uuid).username == 'foobar'
+    assert repo.find_by_uuid('asdf') is None
+
+
+def test_find_account_by_email():
+    account = Account()
+    account.email = 'foobar@gmail.com'
+    repo = AccountRepository()
+    repo.save(account)
+    assert repo.find_by_email(account.email).uuid == account.uuid
+    assert repo.find_by_email('asdf') is None
+
+
+def test_find_account_by_username():
+    account = Account()
+    account.username = 'foobar'
+    repo = AccountRepository()
+    repo.save(account)
+    assert repo.find_by_username(account.username).uuid == account.uuid
+    assert repo.find_by_username('asdf') is None
+
+
+def test_account_authentication():
+    account = Account()
+    account.email = 'foobar@gmail.com'
+    account.password = 'foobar123'
+    repo = AccountRepository()
+    repo.save(account)
+    assert repo.authenticate('foobar@gmail.com', 'foobar123')
+
+
+def test_account_authentication_password_mismatch():
+    account = Account()
+    account.email = 'foobar@gmail.com'
+    account.password = 'foobar123'
+    repo = AccountRepository()
+    repo.save(account)
+    assert not repo.authenticate('foobar@gmail.com', 'asdf42')
+
+
+def test_account_authentication_account_not_found():
+    repo = AccountRepository()
+    assert not repo.authenticate('foobar@gmail.com', 'foobar123')
